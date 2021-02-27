57e2f5c1ee32ca042dde4389fb87d2f79716aed7
Sanjay_Arikatla <sanjay.arikatla@hyperlooptech.com>
2021-02-26 20:20:00 -0800

    more fluid conversions

--- client/src/components/faq/faq.jsx
+++ client/src/components/faq/faq.jsx
@@ -13,40 +13,44 @@ class faq extends Component {
         <br />
         <br />
         <br />
-        <div ref={this.props.refProp} className="faqHeader">{data.title}</div>
-        <div id="main">
-          <div className="container-fluid">
-            <div className="accordion" id="faq">
-              <Question
-                title={data.rows[0].title}
-                content={data.rows[0].content}
-                headid="faqhead1"
-                dataid="faq1"
-              />
-              <Question
-                title={data.rows[1].title}
-                content={data.rows[1].content}
-                headid="faqhead2"
-                dataid="faq2"
-              />
-              <Question
-                title={data.rows[2].title}
-                content={data.rows[2].content}
-                headid="faqhead3"
-                dataid="faq3"
-              />
-              <Question
-                title={data.rows[3].title}
-                content={data.rows[3].content}
-                headid="faqhead4"
-                dataid="faq4"
-              />
-              <Question
-                title={data.rows[4].title}
-                content={data.rows[4].content}
-                headid="faqhead5"
-                dataid="faq5"
-              />
+        <div class="container-fluid pr-3 pl-3">
+          <div ref={this.props.refProp} className="faqHeader">
+            {data.title}
+          </div>
+          <div id="main">
+            <div className="container-fluid">
+              <div className="accordion" id="faq">
+                <Question
+                  title={data.rows[0].title}
+                  content={data.rows[0].content}
+                  headid="faqhead1"
+                  dataid="faq1"
+                />
+                <Question
+                  title={data.rows[1].title}
+                  content={data.rows[1].content}
+                  headid="faqhead2"
+                  dataid="faq2"
+                />
+                <Question
+                  title={data.rows[2].title}
+                  content={data.rows[2].content}
+                  headid="faqhead3"
+                  dataid="faq3"
+                />
+                <Question
+                  title={data.rows[3].title}
+                  content={data.rows[3].content}
+                  headid="faqhead4"
+                  dataid="faq4"
+                />
+                <Question
+                  title={data.rows[4].title}
+                  content={data.rows[4].content}
+                  headid="faqhead5"
+                  dataid="faq5"
+                />
+              </div>
             </div>
           </div>
         </div>
